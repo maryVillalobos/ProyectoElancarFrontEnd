caaa49906e85f9dbbf136f893228b000d55201f4
MaryV <maria.villalobos29@inacapmail.cl>
2024-10-09 17:22:44 -0300

    Subir proyecto inicial desde cero

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'reactstrap'; // Importa Container de Reactstrap para estructurar el layout
+
+const App = () => {
+  return (
+    <Container className="text-center" style={{ paddingTop: '20px' }}>
+      <h1>Hola, funcionó</h1>
+    </Container>
+  );
+};
+
+export default App;
