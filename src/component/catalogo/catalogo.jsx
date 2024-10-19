ec2117c2c243748cf7be0ae0d70fa53e0c8bd6ad
MaryV <maria.villalobos29@inacapmail.cl>
2024-10-19 17:46:29 -0300

    Cambios Catalogo

--- src/component/catalogo/catalogo.jsx
+++ src/component/catalogo/catalogo.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Container, Row } from 'reactstrap'; // Importa Container de Reactstrap para estructurar el layout
 import MyNavbar from '../landingPage/components/MyNavbar';
 import ProductosContainer from './components/Productos/ProductosContainer';
-import Sidebar from './components/Sidebar/sidebar';
+import UpperBar from './components/UpperBar/UpperBar';
 
 const Catalogo = () => {
   return (
     <>
+    <UpperBar/>
       <MyNavbar/>
       <Row>
          <Sidebar/>
